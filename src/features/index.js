4388d2222749966634763cb2225af2c7f084abfb
Damar <damarbm30@gmail.com>
2023-06-28 06:59:27 +0700

    change auth styling

--- src/features/index.js
+++ src/features/index.js
@@ -1,5 +1,6 @@
 import { lazy } from "react";
 
 const Login = lazy(() => import("./auth/Login"));
+const Register = lazy(() => import("./auth/Register"));
 
-export { Login };
+export { Login, Register };
